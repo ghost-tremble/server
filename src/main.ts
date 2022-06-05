42156d3c02bfeaade9f8d4112bb15deb943914c8
ghost-tremble <patokezy@gmail.com>
2022-06-05 08:21:54 +0100

    cha

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
-const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
-});
 
+
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+    resolvers: resolvers,
+  context: (ctx: Context) => ctx,
+  playground: true,
+  introspection: true,
+});
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`
     🚀  Server is ready at ${url}
